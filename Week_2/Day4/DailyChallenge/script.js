68205fa32e727885e95e107f27591db116b9a1bc
Lizgrunfeld <elizabethgrunfeld@gmail.com>
2022-10-23 10:23:00 +0300

    daily challenege update

--- Week_2/Day4/DailyChallenge/script.js
+++ Week_2/Day4/DailyChallenge/script.js
@@ -1,62 +1,64 @@
-// //Prompt the user for several words (separated by commas).
-// Put the words into an array.
-// Console.log the words one per line, in a rectangular frame as seen below.
-// Check out the Hints and Requirements below.
-// For example, if the user gives you:
-// Hello, World, in, a, frame
-// you will transform it to : ["Hello", "World", "in", "a", "frame"]
-// that will get displayed as:
+// // // //Prompt the user for several words (separated by commas).
+// // // Put the words into an array.
+// // // Console.log the words one per line, in a rectangular frame as seen below.
+// // // Check out the Hints and Requirements below.
+// // // For example, if the user gives you:
+// // // Hello, World, in, a, frame
+// // // you will transform it to : ["Hello", "World", "in", "a", "frame"]
+// // // that will get displayed as:
 
-// Hint
-// The number of stars that wraps the sentence, must depend on the length of the longest word.
-
-
-const answer = prompt("Please enter five words for a test and seperate each word with a comma:").split(",")
-const words = answer.splice(",");
+// // Hint
+// // The number of stars that wraps the sentence, must depend on the length of the longest word.
 
 
-// console.log(words);
-const lengthOfLongestWord = getLengthOfLongestWord();
-displayRows();
+// const answer = prompt("Please enter five words for a test and seperate each word with a comma:").split(",")
 
-function displayRows(){
-    const delimiterRow = createdelimiterRow();
-    console.log(delimiterRow);
-for (const word of words){
-    displayWord(word);
-}
-    console.log("delimiterRow:", delimiterRow);
+// // const words = answer.splice(",");
+// //split not working!
+// const words = answer.split(",");
 
-}
 
-function displayWord(word){
-    console.log("* " + word)
-}
+// // console.log(words);
+// const lengthOfLongestWord = getLengthOfLongestWord();
+// displayRows();
 
-words.forEach();
+// function displayRows(){
+//     const delimiterRow = createdelimiterRow();
+//     console.log(delimiterRow);
+// for (const word of words){
+//     displayWord(word);
+// }
+//     console.log("delimiterRow:", delimiterRow);
 
-function createdelimiterRow(){
-    const numberOfStarsOnFirstRow = lengthOfLongestWord +4;   
-    let row ="";
-    for (let i = 0; i < numberOfStarsOnFirstRow; i++) {
-        row = row + "*";
-    }
-}
+// }
 
-function getLengthOfLongestWord(){
-    let lengthOfLongestWord = 0;
-    for (const word of words){
-        console.log(word);
-        const wordLength = word.length
-        if (wordLength > lengthOfLongestWord){
-            lengthOfLongestWord = wordLength; 
-        }
-        console.log("lengthOfTheLongestWord", lengthOfLongestWord)
-    }
-    return lengthOfLongestWord
-}
+// function displayWord(word){
+//     console.log("* " + word )
+// }
 
-// OTHER WAY
+
+// function createdelimiterRow(){
+//     const numberOfStarsOnFirstRow = lengthOfLongestWord +4;   
+//     let row ="";
+//     for (let i = 0; i < numberOfStarsOnFirstRow; i++) {
+//         row = row + "*";
+//     }
+// }
+
+// function getLengthOfLongestWord(){
+//     let lengthOfLongestWord = 0;
+//     for (const word of words){
+//         console.log(word);
+//         const wordLength = word.length
+//         if (wordLength > lengthOfLongestWord){
+//             lengthOfLongestWord = wordLength; 
+//         }
+//         console.log("lengthOfTheLongestWord", lengthOfLongestWord)
+//     }
+//     return lengthOfLongestWord
+// }
+
+// // OTHER WAY
 // let answer = [];
 // for(var i = 0; i < 5; i++)
 //    answer.push(prompt("Enter a word:"))
@@ -66,3 +68,31 @@ function getLengthOfLongestWord(){
 //    console.log(answer[2])
 //    console.log(answer[3])
 //    console.log(answer[4])
+
+
+const answer = prompt("Please enter five words for a test and seperate each word with a comma:").split(",")
+const words= answer.split(",");
+
+
+function findLongestWord(){
+    let max = 0;
+    for (let i = 0; i < words.length; i++){
+        if (words[i].length > max){
+        max = words[i].length;
+        }
+    }
+    return max;
+}
+
+function printWords(words,max){
+    firstAndLast(max);
+    for (let word of words){
+    console.log(`* ${word+ ' '.repeat(max - word.length)}*`);
+}
+}
+firstAndLast(max);
+
+function firstAndLast(max){
+console.log ('*'.repeat(max+4));
+}
+printWords(words,findLongestWord());
