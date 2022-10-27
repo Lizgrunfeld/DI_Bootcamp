1df732231dac6af6062f0354168505ac979da177
Lizgrunfeld <elizabethgrunfeld@gmail.com>
2022-10-27 10:02:42 +0300

    update

--- Week_3/Day_3/dailyChallenge/script.js
+++ Week_3/Day_3/dailyChallenge/script.js
@@ -1,28 +1,44 @@
 // Daily Challenge : Show Only The Letters
 // Create an input type text that takes/shows only letters. (ie. numbers and special characters won’t be accepted)
 
-function mainForm(){
-    var simpleForm = document.createElement("form");
-        simpleForm.setAttribute("method", "get");
-        simpleForm.setAttribute("action", "submit");
 
-    var firstNameField = document.createElement("label")
-        firstNameField.setAttribute("input", "text"),
-        firstNameField.setAttribute("id", "fName"),
-        firstNameField.setAttribute("name", "text"),
+const userInput = document.querySelector("input");
+const allowedChars = /[A-Za-z]/;
+userInput.addEventListener("keydown", checkKey)
 
-        simpleForm.appendChild(firstNameField);
-
-    simpleForm.classList.add("styleForm")
-
-    const section = document.getElementById("place");
-    const myForm = simpleForm
-    section.appendChild(simpleForm)
+function checkKey(event){
+    console.log(event)
+    console.log(event.key)
+    if (!allowedChars.test(event.key)){
+        event.preventDefault();
+    }
 }
-mainForm()
 
-var lettersOnly = document.createElement('input');
-lettersOnly.setAttribute("type","text");
+
+
+
+// function mainForm(){
+//     var simpleForm = document.createElement("form");
+//         simpleForm.setAttribute("method", "get");
+//         simpleForm.setAttribute("action", "submit");
+
+//     var firstNameField = document.createElement("label")
+//         firstNameField.setAttribute("input", "text"),
+//         firstNameField.setAttribute("id", "fName"),
+//         firstNameField.setAttribute("name", "text"),
+
+//         simpleForm.appendChild(firstNameField);
+
+//     simpleForm.classList.add("styleForm")
+
+//     const section = document.getElementById("place");
+//     const myForm = simpleForm
+//     section.appendChild(simpleForm)
+// }
+// mainForm()
+
+// var lettersOnly = document.createElement('input');
+// lettersOnly.setAttribute("type","text");
 
 // Hint: use one of the following events to remove any character that is not a letter
 
