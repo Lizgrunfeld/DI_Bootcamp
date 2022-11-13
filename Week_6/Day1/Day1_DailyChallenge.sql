f2697479151d87c300a03478547d2470097d0df0
Lizgrunfeld <elizabethgrunfeld@gmail.com>
2022-11-13 21:58:02 +0200

    daily Challenge

---
+++ Week_6/Day1/Day1_DailyChallenge.sql
@@ -0,0 +1,7 @@
+SELECT * FROM actors4
+
+INSERT INTO(first_name,last_name,date_birth, number_oscars)
+VALUES
+	('Mike', 'Tyson','1965-02-04',8),
+	('Rebecca','1925-08-01',3),
+	('', 'Gregson','1955-05-05',2);
